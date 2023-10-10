eaea532999bdddb3696ffa9b249f2b9ba52d2285
fmway <fm18lv@gmail.com>
2023-10-10 16:25:49 +0700

    add navbar

---
+++ script.js
@@ -0,0 +1,9 @@
+// Toggle for navbar hamburger
+// -------------------------------------------------------------
+const nav = document.querySelector('#navbar-menu');
+const navbarToggle = document.querySelector("#navbar-toggle");
+
+navbarToggle.addEventListener('click', () => {
+  nav.classList.toggle('hidden');
+});
+// -------------------------------------------------------------
