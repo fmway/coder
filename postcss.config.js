7cadb8c470e6b6881a2d6cdde848928397fd979d
fmway <fm18lv@gmail.com>
2023-10-11 18:31:24 +0700

    using tailwind + postcss for production

---
+++ postcss.config.js
@@ -0,0 +1,13 @@
+const purgecss = require('@fullhuman/postcss-purgecss')
+module.exports = {
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    require('cssnano')({
+      preset: 'default',
+    }),
+    purgecss({
+      content: ['./**/*.html']
+    }),
+  ]
+}
